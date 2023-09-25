2edc748048596959be1c18e6cf04cadf29d6037c
thonyxclusive <123905984+thonyxclusive@users.noreply.github.com>
2023-09-25 11:45:39 +0100

    Increment, decrement and update function

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ let shopItemsData = [{
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
     img: "images/img-4.jpg"
 }]
+
+let basket = [];
 let generateShop =()=>{
     return (shop.innerHTML = shopItemsData.map((x)=>{
         let {id, name, price, desc, img} = x;
@@ -38,9 +40,9 @@ let generateShop =()=>{
             <div class="price-quantity">
                 <h2>$ ${price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
+                    <i onclick = "decrement(${id})" class="bi bi-dash-lg"></i>
                     <div id=${id} class="quantity">0</div>
-                    <i class="bi bi-plus-lg"></i>
+                    <i onclick = "increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
         </div>
@@ -50,3 +52,35 @@ let generateShop =()=>{
 };
 
 generateShop();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+    if (search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    }
+    else {
+        search.item += 1;
+    }
+   // console.log(basket);
+    update(selectedItem.id);
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+    if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    // console.log(basket);
+    update(selectedItem.id);
+};
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+}; 
