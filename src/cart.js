f795990b9d1f62c1c83b639d419fb4d06f53e182
thonyxclusive <123905984+thonyxclusive@users.noreply.github.com>
2023-10-02 13:56:16 +0100

    Cart page

---
+++ src/cart.js
@@ -0,0 +1,30 @@
+let label = document.getElementById("label");
+let shoppingCart = document.getElementById("shopping-cart");
+let basket = JSON.parse(localStorage.getItem("data"))|| [];
+
+console.log(basket);
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, 0);
+
+};
+    calculation ();
+
+    let generateCartItems = () => {
+        if(basket.length !==0){
+            
+        }
+        else {
+            shoppingCart.innerHTML = ``
+            label.innerHTML = `
+            <h2>Cart is empty</h2>
+            <a href="index.html">
+            button.Home
+            </a>
+        
+
+        
+            `
+        }
+    };
--- src/main.js
+++ src/main.js
@@ -1,31 +1,7 @@
 let shop = document.getElementById("shop");
 
 
-let shopItemsData = [{
-    id: "B2005",
-    name: "Casual Shirt",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-    img: "images/img-1.jpg"
-},{
-    id: "B2010",
-    name: "Office Shirt",
-    price: 100,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-    img: "images/img-2.jpg"
-},{
-    id: "B2015",
-    name: "T Shirt",
-    price: 25,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-    img: "images/img-3.jpg"
-},{
-    id: "B2020",
-    name: "Mens Suit",
-    price: 300,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing",
-    img: "images/img-4.jpg"
-}]
+
 
 let basket = JSON.parse(localStorage.getItem("data"))|| [];
 let generateShop =()=>{
